9bb0759d252acefd506668b1c589757777e2bf5f
Nathaniel Beckstead <becksteadn@gmail.com>
2019-07-25 22:28:32 -0400

    debug ip distribution

--- worker/main.py
+++ worker/main.py
@@ -2,7 +2,6 @@ import socket
 import json
 
 def handler(event, context):
-    print(event['Records'][0]['Sns']['Message'])
     message = json.loads(event['Records'][0]['Sns']['Message'])
     host = message['host']
     body = message['body'].encode('ascii')
@@ -10,6 +9,7 @@ def handler(event, context):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((host, 80))
         s.sendall(bytes(body))
-        #data = s.recv(1024)
+        data = s.recv(1024)
 
-    return 0#{'response': str(data)}
+    print(str(data))
+    return {'response': str(data)}
