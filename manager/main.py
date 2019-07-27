afed50ccac0889395145523169c78fbf3e69545b
Nathaniel Beckstead <becksteadn@gmail.com>
2019-07-26 20:48:36 -0400

    loop publishes

--- manager/main.py
+++ manager/main.py
@@ -14,10 +14,20 @@ def handler(event, context):
     #arn = 'arn:aws:sns:us-east-1:358663747217:2fa-swarm'
     arn = os.environ.get('SWARM_SNS_TOPIC_ARN')
     client = boto3.client('sns')
-    response = client.publish(
-        TargetArn=arn,
-        Message=json.dumps(message),
-        MessageStructure='json'
-    )
+
+    if 'times' in event.keys():
+        for _ in range(int(event['times'])):
+            client.publish(
+                TargetArn=arn,
+                Message=json.dumps(message),
+                MessageStructure='json'
+            )
+        response = 'yes'
+    else:
+        response = client.publish(
+            TargetArn=arn,
+            Message=json.dumps(message),
+            MessageStructure='json'
+        )
 
     return {'response': str(response)}
