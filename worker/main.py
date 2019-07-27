df3180cfe9d003a5d482ae530b11602b3ec2de69
Nathaniel Beckstead <becksteadn@gmail.com>
2019-07-27 19:11:39 -0400

    custom port option and error handling

--- worker/main.py
+++ worker/main.py
@@ -7,14 +7,31 @@ def handler(event, context):
     message = json.loads(event['Records'][0]['Sns']['Message'])
     host = message['host']
     body = message['body'].encode('ascii')
+    try:
+        port = int(message['port'])
+    except KeyError:
+        port = 80
 
     resource = boto3.resource('dynamodb')
     table = resource.Table('2FA-Swarm')
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((host, 80))
-        s.sendall(bytes(body))
-        data = s.recv(1024)
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((host, port))
+            s.sendall(bytes(body))
+            data = b' '
+            resp = ''
+            while len(data):
+                data = s.recv(1)
+                if data = b'\r':
+                    break
+                resp += data.decode()
+    except Exception as e:
+        table.put_item(Item={"uuid": str(uuid.uuid4()), "error_msg": str(e)})
+        return {'error_msg': str(e)}
+
+    status_code = int(resp.split(' ')[1])
+
 
     decoded = data.decode()
     obj = json.loads(decoded[decoded.index('{'):decoded.index('}') + 1].strip())
