d20f74f61c216fe2f2cdfc2c7d812f58b93009b7
Nathaniel Beckstead <becksteadn@gmail.com>
2019-07-28 10:14:33 -0400

    go back to synchronous invokes for now

--- manager/main.py
+++ manager/main.py
@@ -1,46 +1,26 @@
 import boto3
 import json
 import os
-import threading
-
-def LaunchThread(threading.Thread):
-    def __init__(self, sns, arn, message):
-        threading.Thread.__init__(self)
-        self.client = sns
-        self.arn = arn
-        self.message = message
-
-    def run(self):
-        self.client.publish(
-            TargetArn=self.arn,
-            Message=self.message,
-            MessageStructure='json'
-        )
 
 def handler(event, context):
     payload = {
-        'default': {
-            'host': event['host'],
-            'body': event['body']
-        }
+        'host': event['host'],
+        'body': event['body']
     }
     message = {
         'default': json.dumps(payload)
     }
 
-    threading_msg = json.dumps(message)
-
-    #arn = 'arn:aws:sns:us-east-1:358663747217:2fa-swarm'
     arn = os.environ.get('SWARM_SNS_TOPIC_ARN')
     client = boto3.client('sns')
 
     if 'times' in event.keys():
         for _ in range(int(event['times'])):
-            try:
-                t = LaunchThread(client, arn, threading_msg)
-                t.start()
-            except Exception as e:
-                print(e)
+                client.publish(
+            TargetArn=arn,
+            Message=json.dumps(message),
+            MessageStructure='json'
+            )
         response = 'success'
     else:
         response = client.publish(
