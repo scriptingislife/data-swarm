1ba10a010e25bd4ff9de83aa2f82d16db8fe9965
Nathaniel Beckstead <becksteadn@gmail.com>
2019-07-27 21:23:15 -0400

    threading for publishing

--- manager/main.py
+++ manager/main.py
@@ -1,15 +1,31 @@
 import boto3
 import json
 import os
+import threading
+
+def LaunchThread(threading.Thread):
+    def __init__(self, sns, arn, message):
+        threading.Thread.__init__(self)
+        self.client = sns
+        self.arn = arn
+        self.message = message
+
+    def run(self):
+        self.client.publish(
+            TargetArn=self.arn,
+            Message=self.message,
+            MessageStructure='json'
+        )
 
 def handler(event, context):
     payload = {
-        'host': event['host'],
-        'body': event['body']
+        'default': {
+            'host': event['host'],
+            'body': event['body']
+        }
     }
-    message = {
-        'default': json.dumps(payload)
-    }
+    message = json.dumps(payload)
+
 
     #arn = 'arn:aws:sns:us-east-1:358663747217:2fa-swarm'
     arn = os.environ.get('SWARM_SNS_TOPIC_ARN')
@@ -17,12 +33,12 @@ def handler(event, context):
 
     if 'times' in event.keys():
         for _ in range(int(event['times'])):
-            client.publish(
-                TargetArn=arn,
-                Message=json.dumps(message),
-                MessageStructure='json'
-            )
-        response = 'yes'
+            try:
+                t = LaunchThread(client, arn, message)
+                t.start()
+            except Exception as e:
+                print(e)
+        response = 'success'
     else:
         response = client.publish(
             TargetArn=arn,
